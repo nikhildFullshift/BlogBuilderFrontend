0535def2cb588ce2d252a4f515fd58433a08d770
Nikhil Dash <nikhild@mindfiresolutions.com>
2023-07-21 11:51:29 +0530

    Search bar fixes

--- src/components/BlogSearchList.tsx
+++ src/components/BlogSearchList.tsx
@@ -11,7 +11,7 @@ import {
 import { CardActionArea } from "@mui/material";
 import SearchBarAligned from "./search-bar/SearchBarAligned";
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import moment from "moment";
 
 export default function BlogSearchList() {
@@ -24,17 +24,21 @@ export default function BlogSearchList() {
 
   const navigate = useNavigate();
 
-  const handleSearchTerm = (event: any) => {
+  const handleSearchTerm = async (event: any) => {
     setSearchTerm(event.target.value);
     if (event.keyCode === 13) {
-      navigate(`/search?title=${searchTerm}`);
+      if (!searchTerm) {
+        return navigate("/");
+      }
+      window.location.href = `/search?title=${searchTerm}`;
+      // navigate({
+      //   pathname: "/search",
+      //   search: `?title=${searchTerm}`,
+      // });
     }
   };
 
   const fetchResults = async (searchTerm: any) => {
-    if (!searchTerm) {
-      return navigate("/");
-    }
     const config = {
       method: "GET",
       headers: {
@@ -48,23 +52,23 @@ export default function BlogSearchList() {
       config
     );
     result = await result.json();
+    if (result.data.hits.hits.length === 0) {
+      navigate("/");
+    }
     setSearchResults(result.data.hits.hits);
-    console.log("RESULT", result);
   };
 
   useEffect(() => {
-    if (!searchTerm && initialLoad) {
-      setSearchTerm(searchParams.get("title") || "");
-      setInitialLoad(false);
+    const title = searchParams.get("title") || "";
+    setSearchTerm(title);
+    if (!title) {
+      return navigate("/");
     }
-    const debounceId = setTimeout(() => {
-      fetchResults(searchTerm);
-    }, 1000);
 
-    return () => {
-      clearTimeout(debounceId);
-    };
-  }, [searchTerm]);
+    (async () => {
+      await fetchResults(title);
+    })();
+  }, []);
 
   return (
     <Container>
@@ -74,81 +78,85 @@ export default function BlogSearchList() {
       />
       {searchResults.map((item: any) => {
         return (
-          <Card
-            key={item._id}
-            sx={{
-              maxWidth: "53%",
-              [theme.breakpoints.down("sm")]: {
-                maxWidth: "100%",
-              },
-              marginBottom: "10px",
-            }}
-          >
-            <CardActionArea>
-              <CardContent>
-                <div
-                  style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                  }}
-                >
-                  {!item._source._tags &&
-                    item._source.tags?.map((tagItem: any) => (
-                      <Chip label={tagItem} size="small" variant="outlined" />
-                    ))}
-                </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                  <div style={{ flex: 3 }}>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {item._source.title}
-                    </Typography>
-                    {item._source.meta_description && (
-                      <Typography variant="body2" color="text.secondary">
-                        {item._source.meta_description}
-                      </Typography>
-                    )}
+          <Link to={`/blog/view/${item._source.id}`}>
+            <Card
+              key={item._id}
+              sx={{
+                maxWidth: "53%",
+                [theme.breakpoints.down("sm")]: {
+                  maxWidth: "100%",
+                },
+                marginBottom: "10px",
+              }}
+            >
+              <CardActionArea>
+                <CardContent>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexWrap: "wrap",
+                    }}
+                  >
+                    {!item._source._tags &&
+                      item._source.tags?.map((tagItem: any) => (
+                        <Chip label={tagItem} size="small" variant="outlined" />
+                      ))}
                   </div>
-                  <img
-                    style={{ flex: 1, borderRadius: "3%", height: "100px" }}
-                    src="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-                    alt="green iguana"
-                  />
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    marginTop: "15px",
-                    justifyContent: "space-between",
-                    alignItems: "end",
-                  }}
-                >
-                  <div style={{ display: "flex" }}>
-                    <div>
-                      <Avatar
-                        src={"https://i.pravatar.cc/300?img=13"}
-                        style={{ marginRight: "7px" }}
-                      />
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <div style={{ flex: 3 }}>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {item._source.title}
+                      </Typography>
+                      {item._source.meta_description && (
+                        <Typography variant="body2" color="text.secondary">
+                          {item._source.meta_description}
+                        </Typography>
+                      )}
+                    </div>
+                    <img
+                      style={{ flex: 1, borderRadius: "3%", height: "100px" }}
+                      src="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+                      alt="green iguana"
+                    />
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      marginTop: "15px",
+                      justifyContent: "space-between",
+                      alignItems: "end",
+                    }}
+                  >
+                    <div style={{ display: "flex" }}>
+                      <div>
+                        <Avatar
+                          src={"https://i.pravatar.cc/300?img=13"}
+                          style={{ marginRight: "7px" }}
+                        />
+                      </div>
+                      <div>
+                        <Box sx={{ typography: "subtitle2" }}>
+                          {item._source.author_name
+                            ? item._source.author_name
+                            : "Annonymous"}
+                        </Box>
+                        <Typography variant="body2" color="text.secondary">
+                          {moment(item._source.created_at).format(
+                            "MMM DD YYYY"
+                          )}
+                        </Typography>
+                      </div>
                     </div>
                     <div>
-                      <Box sx={{ typography: "subtitle2" }}>
-                        {item._source.author_name
-                          ? item._source.author_name
-                          : "Annonymous"}
-                      </Box>
                       <Typography variant="body2" color="text.secondary">
-                        {moment(item._source.created_at).format("MMM DD YYYY")}
+                        Like: 123
                       </Typography>
                     </div>
                   </div>
-                  <div>
-                    <Typography variant="body2" color="text.secondary">
-                      Like: 123
-                    </Typography>
-                  </div>
-                </div>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Link>
         );
       })}
     </Container>
